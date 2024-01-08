9ad639c6aa2bbf1f0c076a511ea2797be779a667
fihar abdelali <fiharali0@gmail.com>
2024-01-08 11:59:29 +0100

    add uml

--- app/controller/AuthController.php
+++ app/controller/AuthController.php
@@ -2,140 +2,155 @@
 
 namespace app\controller;
 
-
 include __DIR__ . '/../../vendor/autoload.php';
 
 use app\model\User;
-use app\model\Annonce;
 
 session_start();
+
+
+
+// spl_autoload_register(function ($class) {
+//     include  __DIR__ .'/../'.$class.'.php'; 
+// });
+
+
+
+
+
 
 
 class AuthController
 {
 
-    // public function landing(){
-
-    //     $annonce = new Annonce(null, null,null,null,null,null);
-    //      $annonces = $annonce->getAnnonces();
-    //     include "../../views/client/landing.php";
-    // }
 
 
-    // public function Register()
+    public function Register($name, $email, $password, $confirm_password)
+    {
+        // return  $this->check($email);
+        // session_start();
+
+        if (empty($name)) {
+            $_SESSION['name'] = "Name is required";
+        } elseif (strlen($name) < 3) {
+            $_SESSION['name'] = "Name must be at least 3 characters";
+        } else {
+            $_SESSION['name'] = "";
+        }
+        if (empty($email)) {
+            $_SESSION['email'] = "email is required";
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $_SESSION['email'] = "email must be valid";
+        } else {
+            $_SESSION['email'] = "";
+        }
+
+        if (empty($password)) {
+            $_SESSION['password'] = "password is required";
+        } elseif (strlen($password) < 7) {
+            $_SESSION['password'] = "password  must be >= 8";
+        } else {
+            $_SESSION['password'] = "";
+        }
+        if ($password != $confirm_password) {
+            $_SESSION['confirm_password'] = "password doesn't match";
+        } else {
+            $_SESSION['confirm_password'] = "";
+        }
+        // $checkUser = new User(null, $email, null);
+        // $check = $checkUser->CheckUser();
+        // if (count($check)  > 0) {
+        //     $_SESSION['email'] = "email exist ";
+        // }
+
+
+        if (empty($_SESSION['name']) &&  empty($_SESSION['email']) && empty($_SESSION['password']) && empty($_SESSION['confirm_password'])) {
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            // $checkUser = new User($name, $email, $password);
+            // $lastInsertId=$checkUser->createUser();
+            $_SESSION['username'] = $name;
+            $_SESSION['isAdmin'] = false;
+            // $_SESSION['id'] = $lastInsertId;
+            header("location:../../views/client/home/index.php");
+            exit();
+        } else {
+            header("location:../../views/auth/register.php");
+            exit();
+        }
+    }
+
+
+    public function login($email, $password)
+    {
+
+        if (empty($email)) {
+            $_SESSION['email'] = "email is required";
+        } else {
+            $_SESSION['email'] = "";
+        }
+
+        if (empty($password)) {
+            $_SESSION['password'] = "password is required";
+        } else {
+            $_SESSION['password'] = "";
+        }
+
+        // if (empty($_SESSION['email']) &&  empty($_SESSION['password'])) {
+        //     // $checkUser = new User(null, $email, $password);
+        //     // $check = $checkUser->CheckUser();
+        //     if (count($check)  > 0) {
+        //         $user = $check[0];
+        //         if (password_verify($password, $user["password"])) {
+        //             // header("location:../../views/produit/index.php");
+        //             //    var_dump( $user["name"]);
+        //             $_SESSION['username'] = $user["firstName"];
+        //             $_SESSION['id'] = $user["id"];
+        //             if ($user["name"] == "admin") {
+        //                 $_SESSION['isAdmin'] = true;
+        //                 header("location:../../views/admin/books/index.php");
+        //             } else {
+        //                 $_SESSION['isAdmin'] = false;
+        //                 header("location:../../views/client/home/index.php");
+        //             }
+        //         } else {
+        //             $_SESSION['password'] = "password is incorrect";
+        //             header("location:../../views/auth/login.php");
+        //         }
+        //     } else {
+        //         $_SESSION['email'] = "email  doesn't exist ";
+        //         header("location:../../views/auth/login.php");
+        //     }
+        // } else {
+        //     header("location:../../views/auth/login.php");
+        //     exit();
+        // }
+    }
+
+    // public function AllUsers()
     // {
-    //     $email = $_POST['email'];
-    //     $firstname = $_POST['firstname'];
-    //     $lastname = $_POST['lastname'];
-    //     $password = $_POST['password'];
 
-    //     $password = password_hash($password, PASSWORD_DEFAULT);
-    //     $objuser = new User(null, $firstname, $lastname, $email, $password, null, null);
-    //     $objuser->createUser();
-
-    //     $_SESSION['email'] = $email;
-    //     $_SESSION['firstname'] = $firstname;
-    //     $_SESSION['lastname'] = $lastname;
-    //     $_SESSION['password'] = $password;
-
-    //     header('location:../login   ');
+    //     $allUsers = new User(null, null, null);
+    //     return   $allUsers->getAllUsers();
+    //     // var_dump($allUsers->getAllUsers());
     // }
-
-
-    // public function login()
-    // {
-
-    //     $email = $_POST['email'];
-    //     $password = $_POST['password'];
-    //     if (empty($email) || empty($password)) {
-    //         echo "von avez pas enregistrer le nom et prenom";
-    //     } else {
-    //         $obj = new User(null, null, null, $email, $password, null, null);
-    //         $data = $obj->getUserByUsername();
-    //     }
-
-    //     if (empty($data)) {
-    //         echo "email not on data base";
-    //     } elseif (password_verify($password, $data->password)) {
-
-    //         $_SESSION['email'] = $email;
-    //         $_SESSION['role'] = $data->role_name;
-    //         $_SESSION['id'] = $data->id;
-    //         if ($data->role_name == 'admin') {
-    //             echo "admin";
-    //         } elseif ($data->role_name == 'user') {
-    //             echo "user";
-    //             header('location: client/landing');
-    //         } elseif ($data->role_name == 'seller') {
-    //             echo "seller";
-    //             header('location:../Profile');
-    //         }
-    //     }
-    // }
-
-
-    public static function AllUsers()
-    {
-        $allUsers = new User(null, null, null, null, null, null, null);
-        return   $allUsers->getAllUsers();
-    }
-    public static function updateUser()
-    {
-
-        $id = $_POST['id'];
-        $firstname = $_POST["firstname"];
-        $lastname = $_POST["lastname"];
-        $email = $_POST["email"];
-        $phone = $_POST['phone'];
-        $profile = $_POST['profile'];
-        $emailHiden = $_POST['emailHiden'];
-
-        User::updateUser($id, $firstname, $lastname, $email, null, $phone, $profile, $emailHiden);
-        header('location:../../Profile');
-    }
-
-
-
-
-    public  function showUserByEmail()
-    {
-        $email = $_SESSION['email'];
-        $userModel = new User(null, null, null, $email, null, null, null);
-        $user = $userModel->getUserByEmail($email)[0];
-        include '../../views/client/sellerProfileEdit.php';
-    }
-    public  function deleteUser()
-    {
-        $id = $_GET["id"];
-        $user = new User($id, null, null, null, null, null, null);
-        $user->delete();
-        header('location:../users');
-    }
-
-
-    public  function test()
-    {
-        echo "test";
-        echo "<a href='/wiki2/user/client/landing'>test2</a>";
-    }
-    public  function test_test()
-    {
-        echo "test2";
-        echo "<a href='/wiki2/user/client/detail/annonce'>test3</a>";
-    }
-    public  function test_test_test()
-    {
-        echo "<a href='/wiki2/user/client/landing'>test2</a>";
-        echo "<a href='/'>test2</a>";
-    }
-
-    public function login(){
-        include_once '../../views/auth/login.php';
-        exit();
-    }
-
-    public function register(){
-        include_once '../../views/auth/register.php';
-        exit();
-    }
 }
+
+
+
+
+
+if (isset($_POST['register'])) {
+    extract($_POST);
+    $registerController = new AuthController();
+    $registerController->Register($name, $email, $password, $confirm_password);
+}
+
+if (isset($_POST['login'])) {
+    extract($_POST);
+    $registerController = new AuthController();
+    $registerController->login($email,  $password);
+}
+
+
+$getAllUsers = new AuthController();
+// $registerController->AllUsers();
--- app/model/User.php
+++ app/model/User.php
@@ -55,83 +55,14 @@ class User
         $this->phone = $phone;
         $this->profile = $profile;
     }
-    public  function getUserByEmail($email)
-    {
-        $sql = "SELECT users.*, roles.name as role FROM users
-        LEFT JOIN roles ON users.role_id = roles.id
-        WHERE users.email = :email";
-        $statement = $this->db->prepare($sql);
-        $statement->bindParam(':email', $email, PDO::PARAM_STR);
-        if ($statement) {
-            $statement->execute();
-            $resultInstances = $statement->fetchAll(PDO::FETCH_ASSOC);
-                return $resultInstances;
-            } else {
-                return null;
-            }
-        }
+  
 
     
 
-    public function createUser()
-    {
+ 
 
-        $query = "INSERT INTO `users` (firstname, lastname, email, password,role_id) VALUES (?, ?, ?, ?,3 )";
-
-        $stmt = $this->db->prepare($query);
-
-        $stmt->bindParam(1, $this->firstname);
-        $stmt->bindParam(2, $this->lastname);
-        $stmt->bindParam(3, $this->email);
-        $stmt->bindParam(4, $this->password);
-        $stmt->execute();
-
-    }
-
-    public static function getAllUsers()
-    {
-        $sql = "SELECT users.*, roles.name as rolename FROM users
-        LEFT JOIN roles ON users.role_id = roles.id";
-        $db = Connection::getInstence()->getConnect();
-        $statement = $db->prepare($sql);
-        if ($statement) {
-            $statement->execute();
-            $resultInstances = $statement->fetchAll(PDO::FETCH_OBJ);
-            return $resultInstances;
-            // if ($resultInstances) {
-            //     $users = [];
-            //     foreach ($resultInstances as $key => $result) {
-            //         $userInstance = new User($result['id'], $result['firstname'], $result['lastname'],
-            //             $result['email'], $result['password'], $result['phone'], $result['profile']);
-            //         $users[] = $userInstance;
-            //     }
-            //     return $users;
-            // } else {
-            //     return null;
-            // }
-        }
-    }
-    public static function updateUser($id,$firstname , $lastname,$email , $password , $phone , $profile,$emailHiden)
-    {
-        $db = Connection::getInstence()->getConnect();
-        $select = "SELECT * FROM users WHERE email = ?";
-        $statemnt2 = $db->prepare($select);
-        $statemnt2->execute([$emailHiden]);
-        $userInfo = $statemnt2->fetchAll(PDO::FETCH_ASSOC);
-        $idUser = $userInfo[0]['id'];
-        $sql = "UPDATE users SET firstname = :firstname, lastname = :lastname, email = :email, password = :password,
-            phone = :phone, profile = :profile WHERE id = :idUser";
-        $statemnt = $db->prepare($sql);
-        $statemnt->bindParam(':firstname', $firstname, PDO::PARAM_STR);
-        $statemnt->bindParam(':lastname', $lastname, PDO::PARAM_STR);
-        $statemnt->bindParam(':email', $email, PDO::PARAM_STR);
-        $statemnt->bindParam(':password', $password, PDO::PARAM_STR);
-        $statemnt->bindParam(':phone', $phone, PDO::PARAM_INT);
-        $statemnt->bindParam(':profile', $profile, PDO::PARAM_INT);
-        $statemnt->bindParam(':idUser', $idUser, PDO::PARAM_INT);
-        $statemnt->execute();
-        $_SESSION['email']=$email;
-    }
+ 
+   
 
     public function delete()
     {
@@ -141,16 +72,7 @@ class User
 
     }
 
-    public function getUserByUsername(){ 
-        $sql="SELECT u.*, r.name AS role_name FROM users as u
-        INNER JOIN roles as r ON r.id = u.role_id WHERE u.email =? ";
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute([$this->email]);
-        $row = $stmt->fetch(PDO::FETCH_OBJ);
-        // $stmt->closeCursor();
-        return $row;
-        
-    }
+   
 
 
 }
